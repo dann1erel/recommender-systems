d6d25a46d4c911a709d0e4f273eb26ed1ac13985
ilya-shlom <ilyashlom32@gmail.com>
2022-05-03 17:55:52 +0300

    File reading

--- collaborative-system.py
+++ collaborative-system.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
+from config import settings
 
-ratings_df = pd.read_csv('ml-latest-small/ml-latest-small/ratings.csv')
+ratings_df = pd.read_csv(settings['collab_file'])
 print('Unique users count: {}'.format(len(ratings_df['userId'].unique())))
 print('Unique movies count: {}'.format(len(ratings_df['movieId'].unique())))
 print('DataFrame shape: {}'.format(ratings_df.shape))
