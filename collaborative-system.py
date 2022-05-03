ec0586c945b39c15eaf0d49fc83bfb1745a4898a
dann1erel <dannierel0@gmail.com>
2022-05-03 17:52:11 +0300

    added some code

--- collaborative-system.py
+++ collaborative-system.py
@@ -1,3 +1,9 @@
 import numpy as np
+import pandas as pd
 
-print("Hello, World!")
+ratings_df = pd.read_csv('ml-latest-small/ml-latest-small/ratings.csv')
+print('Unique users count: {}'.format(len(ratings_df['userId'].unique())))
+print('Unique movies count: {}'.format(len(ratings_df['movieId'].unique())))
+print('DataFrame shape: {}'.format(ratings_df.shape))
+
+ratings_df.head()
