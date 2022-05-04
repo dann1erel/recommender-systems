85394ba284ac293a955181f016a35c35d807ed6f
ilya-shlom <ilyashlom32@gmail.com>
2022-05-04 13:49:12 +0300

    ratings_df -> ratings_df_sample

--- collaborative-system.py
+++ collaborative-system.py
@@ -4,7 +4,7 @@ from config import settings
 
 ratings_df = pd.read_csv(settings['collab_file'])
 
-n = 100000
+n = 10000
 ratings_df_sample = ratings_df[:n]
 
 n_users = len(ratings_df_sample['userId'].unique())
@@ -15,10 +15,10 @@ print('Unique movies count: {}'.format(len(ratings_df_sample['movieId'].unique()
 print('DataFrame shape: {}'.format(ratings_df_sample.shape))
 print("# {}".format(ratings_df.head()))
 
-lst = np.array([[0.0]*ratings_df['movieId'].max()]*len(ratings_df['userId'].unique()))
+lst = np.array([[0.0]*ratings_df_sample['movieId'].max()]*len(ratings_df_sample['userId'].unique()))
 
-for x in range(ratings_df.shape[0]):
-    lst[tuple(ratings_df['userId'])[x]-1][tuple(ratings_df['movieId'])[x]-1] = tuple(ratings_df['rating'])[x]
+for x in range(ratings_df_sample.shape[0]):
+    lst[tuple(ratings_df_sample['userId'])[x]-1][tuple(ratings_df_sample['movieId'])[x]-1] = tuple(ratings_df_sample['rating'])[x]
 
 print("\nRatings:")
 print(*lst, sep='\n')
@@ -31,10 +31,10 @@ def cos_similarity(vecA, vecB):
     return result
 
 
-lst_sim = np.array([[0.0]*len(ratings_df['userId'].unique())]*len(ratings_df['userId'].unique()))
+lst_sim = np.array([[0.0]*len(ratings_df_sample['userId'].unique())]*len(ratings_df_sample['userId'].unique()))
 
-for i in range(len(ratings_df['userId'].unique())):
-    for j in range(len(ratings_df['userId'].unique())):
+for i in range(len(ratings_df_sample['userId'].unique())):
+    for j in range(len(ratings_df_sample['userId'].unique())):
         if i != j:
             lst_sim[i][j] = cos_similarity(np.array(lst[i]), np.array(lst[j]))
 
