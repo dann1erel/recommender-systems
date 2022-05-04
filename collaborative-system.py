dda1ce265f668b3f0fbd9ab47e2c019582aec64f
ilya-shlom <ilyashlom32@gmail.com>
2022-05-04 15:13:06 +0300

    added user

--- collaborative-system.py
+++ collaborative-system.py
@@ -4,8 +4,9 @@ from config import settings
 
 ratings_df = pd.read_csv(settings['collab_file'])
 
-n = 10000
+n = 1000
 ratings_df_sample = ratings_df[:n]
+user_ratings = [0.0, 4.0, 3.0, 5.0, 1.0]
 
 n_users = len(ratings_df_sample['userId'].unique())
 n_movies = len(ratings_df_sample['movieId'].unique())
@@ -13,29 +14,28 @@ n_movies = len(ratings_df_sample['movieId'].unique())
 print('Unique users count: {}'.format(len(ratings_df_sample['userId'].unique())))
 print('Unique movies count: {}'.format(len(ratings_df_sample['movieId'].unique())))
 print('DataFrame shape: {}'.format(ratings_df_sample.shape))
-print("# {}".format(ratings_df.head()))
+print("# {}".format(ratings_df.head(n=15)))
 
 lst = np.array([[0.0]*ratings_df_sample['movieId'].max()]*len(ratings_df_sample['userId'].unique()))
 
 for x in range(ratings_df_sample.shape[0]):
-    lst[tuple(ratings_df_sample['userId'])[x]-1][tuple(ratings_df_sample['movieId'])[x]-1] = tuple(ratings_df_sample['rating'])[x]
+    lst[tuple(ratings_df_sample['userId'])[x]-1][tuple(ratings_df_sample['movieId'])[x]-1]\
+        = tuple(ratings_df_sample['rating'])[x]
 
 print("\nRatings:")
 print(*lst, sep='\n')
+print("\nUser ratings: ", user_ratings)
 
-ratings_df.head()
 
-
-def cos_similarity(vecA, vecB):
-    result = (np.dot(vecA, vecB))/(np.linalg.norm(vecA) * np.linalg.norm(vecB))  # dot - scalar product, norm - vector length???
+def cos_similarity(vec_a, vec_b):
+    # dot - scalar product, norm - vector length???
+    result = (np.dot(vec_a, vec_b))/(np.linalg.norm(vec_a) * np.linalg.norm(vec_b))
     return result
 
 
-lst_sim = np.array([[0.0]*len(ratings_df_sample['userId'].unique())]*len(ratings_df_sample['userId'].unique()))
+lst_sim = np.array([0.0]*len(ratings_df_sample['userId'].unique()))
 
 for i in range(len(ratings_df_sample['userId'].unique())):
-    for j in range(len(ratings_df_sample['userId'].unique())):
-        if i != j:
-            lst_sim[i][j] = cos_similarity(np.array(lst[i]), np.array(lst[j]))
+            lst_sim[i] = cos_similarity(user_ratings, np.array(lst[i]))
 
 print("\nSimilarity matrix:\n", lst_sim)
