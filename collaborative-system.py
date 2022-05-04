5f893025496c373c00ebc51a3011998488f313e3
ilya-shlom <ilyashlom32@gmail.com>
2022-05-04 12:27:26 +0300

    Similarity matrix

--- collaborative-system.py
+++ collaborative-system.py
@@ -4,8 +4,6 @@ from config import settings
 
 ratings_df = pd.read_csv(settings['collab_file'])
 
-
-
 n = 100000
 ratings_df_sample = ratings_df[:n]
 
@@ -22,10 +20,22 @@ lst = np.array([[0.0]*ratings_df['movieId'].max()]*len(ratings_df['userId'].uniq
 for x in range(ratings_df.shape[0]):
     lst[tuple(ratings_df['userId'])[x]-1][tuple(ratings_df['movieId'])[x]-1] = tuple(ratings_df['rating'])[x]
 
+print("\nRatings:")
 print(*lst, sep='\n')
 
 ratings_df.head()
 
+
 def cos_similarity(vecA, vecB):
     result = (np.dot(vecA, vecB))/(np.linalg.norm(vecA) * np.linalg.norm(vecB))  # dot - scalar product, norm - vector length???
     return result
+
+
+lst_sim = np.array([[0.0]*len(ratings_df['userId'].unique())]*len(ratings_df['userId'].unique()))
+
+for i in range(len(ratings_df['userId'].unique())):
+    for j in range(len(ratings_df['userId'].unique())):
+        if i != j:
+            lst_sim[i][j] = cos_similarity(np.array(lst[i]), np.array(lst[j]))
+
+print("\nSimilarity matrix:\n", lst_sim)
