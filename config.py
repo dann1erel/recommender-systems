b81bd28afc0998aa75064c35822e31c63015c557
ilya-shlom <ilyashlom32@gmail.com>
2022-05-12 17:51:00 +0300

    we have successfully divided words.

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 settings = {
-    'collab_file': 'ratings_small.csv'
+    'collab_file': 'ratings_small.csv',
+    'content_file': 'df_text_eng.csv'
 }
--- content-based.py
+++ content-based.py
@@ -1,6 +1,26 @@
 import numpy as np
 import pandas as pd
 from config import settings
+import re
 from stop_words import get_stop_words
 
-print()
+text_df = pd.read_csv(settings['content_file'])
+text_df_sample = text_df[:10000]
+
+print(*get_stop_words(language="en"), sep='\n')
+
+splitters = [';', ',', ' ', ';', ':']
+
+words = set()
+for x in text_df_sample["blurb"]:
+    text = ''.join(i if (i.isalnum()) or (i == " ") else " " for i in x)
+    text = text.split()
+    for w in text:
+        # w = w.replace(",", '').replace()
+        words.add(w)
+
+for x in get_stop_words(language="en"):
+    words.discard(x)
+words = sorted(words)
+
+print(words)
