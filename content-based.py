daf1e7dd4cc416627598baaac414c8b4515446f1
Tema <89fpsmall@gmail.com>
2022-05-12 20:52:49 +0300

    add (amount in text, amount of success)

--- content-based.py
+++ content-based.py
@@ -6,18 +6,24 @@ from stop_words import get_stop_words as sw
 text_df = pd.read_csv(settings['content_file'])
 text_df_sample = text_df[:10000]
 
+texts = list(text_df_sample['blurb'])
+states = list(text_df_sample['state'])
+
 words = {}
-for x in text_df_sample["blurb"]:
-    text = ''.join(i if (i.isalnum()) or (i == " ") else " " for i in x).split()
+for ind in range(text_df_sample.shape[0]):
+    sent = texts[ind]
+    text = ''.join(s if s.isalnum() else " " for s in sent).split()
     for w in text:
         if len(w) > 1:
             w = w.lower()
+            state = 1 if states[ind] == 'successful' else 0
             if w in words:
-                words[w] += 1
+                words[w][0] += 1
+                words[w][1] += state
             else:
-                words.update({w: 1})
+                words.update({w: [1, state]})
 
 for x in sw(language='en'):
     words.pop(x, None)
 
-print(sorted(words.items(), key=lambda el: el[1], reverse=True))
+print(sorted(words.items(), key=lambda el: el[1][0], reverse=True))
